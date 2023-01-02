294044268ab077be2aa29133afca3103dc834dec
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2023-01-02 15:18:16 +0530

    Update xcat_customize_alma-8.7.sh

--- xcat_customize_alma-8.7.sh
+++ xcat_customize_alma-8.7.sh
@@ -16,7 +16,7 @@ sed -i 's/rocky/alma/g' /opt/xcat/lib/perl/xCAT/Utils.pm
 sed -i 's/rocky/alma/g' /opt/xcat/lib/perl/xCAT_plugin/anaconda.pm
 sed -i 's/Rocky/Alma/g' /opt/xcat/lib/perl/xCAT_plugin/anaconda.pm
 sed -i 's/rocky8.5/alma8.7/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm
-sed -i 's/1636882174.934804/1652294731.711015/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #dvd.iso
+sed -i 's/1636882174.934804/1668064303.058266/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #dvd.iso
 sed -i 's/rocky8.4/alma8.7/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm
 sed -i 's/1624205633.869423/1652294731.711601/g' /opt/xcat/lib/perl/xCAT/data/discinfo.pm    #minimal.iso
 
