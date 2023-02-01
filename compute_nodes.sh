bc46b20107fe8a0659ce400cd8758023d3082a90
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2023-02-01 18:33:30 +0530

    Update compute_nodes.sh

--- compute_nodes.sh
+++ compute_nodes.sh
@@ -32,6 +32,11 @@ echo "Enter the ip(in dhcp network) for cn:"
 read ip
 echo "Enter the mac address of compute nodes:"
 read mac_add
+full_domain_name=$(hostname.$domain)
+dhcp_interface_ip= $(ip r |grep $dhcpinterface  |grep -oP "(\d+\.){3}\d+"|sed -n '2 p')
+cat >> /etc/hosts <<EOF
+dhcp_interface_ip  $full_domain_name
+EOF
 
 mkdef -t node $cn groups=compute,all cons=ipmi ip=$ip netboot=xnba installnic=mac primarynic=mac mac=$mac_add postscripts="confignetwork -s"
 
